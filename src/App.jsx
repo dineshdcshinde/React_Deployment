03260be2b84afcc94c172e42e3861b56cffaa032
Dinesh_DC <dineshshinde81563@gmail.com>
2024-11-19 13:09:47 +0530

    Learning deployment of vite_react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  const KeyGPT = import.meta.env.VITE_React_App_chatGPT;
+  const KeyClaude = import.meta.env.VITE_React_App_Claude;
+  return (
+    <>
+    <p>Your gpt key is {KeyGPT}</p>
+    <p>Your claude key is {KeyClaude}</p>
+    </>
+  )
+}
+
+export default App
